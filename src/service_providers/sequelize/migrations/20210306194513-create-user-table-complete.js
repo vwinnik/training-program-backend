32929f1ab5410508bc6dc592e7a6f4fbb1b55f1e
vwinnik <winnikvanina@gmail.com>
2021-03-06 17:44:05 -0300

    Implement create-user-table-complete migration

---
+++ src/service_providers/sequelize/migrations/20210306194513-create-user-table-complete.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user');
+  }
+};
