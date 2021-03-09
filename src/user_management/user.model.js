8c24360fb562457e141d039b3873f358f97d842e
vwinnik <winnikvanina@gmail.com>
2021-03-09 20:08:38 -0300

    Implement  user model and resolvers

--- src/user_management/user.model.js
+++ src/user_management/user.model.js
@@ -1,11 +1,37 @@
+import bcrypt from 'bcrypt'
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  })
+  }, { timestamps: false })
 
+  User.Prototype.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(9))
+  }
+  User.Prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password)
+  }
+  
   return User
 }
--- src/user_management/user.resolver.js
+++ src/user_management/user.resolver.js
@@ -2,8 +2,22 @@ import models from '~/src/service_providers/sequelize/models'
 
 export default {
   Mutation: {
-    createUser: () => {
-      return models.user.create()
+    createUser: (_,{input}) => {
+      return models.user.create({
+        firstName: input.firstName,
+        lastName: input.lastName,
+        email: input.email,
+        password: input.password
+      })
+    }
+  },
+  Query: {
+    logInUser: (email, password) => {
+      var pass = models.user.generateHash(password);
+      return models.user.find(user => user.email === email && user.password === pass);
+    },
+    users: () => {
+      return models.users.findAll();
     }
   }
 }
